d18e328a803910c6b1d0d9785cf8b716fa160497
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2026-08-17 13:01:12 -0300

    .

--- mux.sv
+++ mux.sv
@@ -4,11 +4,12 @@ module mux
     input  logic [31:0] a, b, c, d,
     input  logic sel1, sel2
   ); 
-
-  and g1 [31:0] (f1, a, n_sel1),
-      g2 [31:0] (f2, b, sel1);
-      g3 [31:0] (f3, c, n_sel2),
-      g4 [31:0] (f4, d, sel2);
+  wire [31:0] f1, f2, f3, f4;
+  wire n_sel1, n_sel2;
+  and g1 [31:0] (f1, a, n_sel1, n_sel2),
+      g2 [31:0] (f2, b, n_sel1, sel2),
+      g3 [31:0] (f3, c, sel1, n_sel2),
+      g4 [31:0] (f4, d, sel1, sel2);
   or  g5 [31:0] (f, f1, f2, f3, f4);
   not g6(n_sel1, sel1);
   not g7(n_sel2, sel2);
--- tb_mux.sv
+++ tb_mux.sv
@@ -4,10 +4,10 @@ module tb_mux;
   logic [1:0] count;
   logic [31:0] muxOut;
 
-  mux dut(.f(muxOut), .a(400), .b(300), .c(200), .d(100), .sel1(count[1]), .sel2(count[0]));
+  mux dut(.f(muxOut[31:0]), .a(400), .b(300), .c(200), .d(100), .sel1(count[1]), .sel2(count[0]));
 
   initial begin
-    $monitor($time,"a = %d | b = %d | c = %d | d = %d | sel1 = %b | sel2 = %b | muxOut = %d", 400, 300, 200, 100, count[1], count[0], muxOut);
+    $monitor($time,"a = %d | b = %d | c = %d | d = %d | sel1 = %b | sel2 = %b | muxOut = %d", 400, 300, 200, 100, count[1], count[0], muxOut[31:0]);
     for(count = 0; count != 2'b11; count++) #10;     
     #10 $stop;
   end
