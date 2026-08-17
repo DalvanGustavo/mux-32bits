7533fe1e3aac6d995a3ece14e00b6bd6e9aa8a06
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2026-08-17 12:37:52 -0300

    .

--- mux.sv
+++ mux.sv
@@ -5,16 +5,12 @@ module mux
     input  logic sel1, sel2
   ); 
 
-
-  generate
-  for (i = 0; i < 32; i++) begin
-    and g1(f1, a, n_sel1),
-        g2(f2, b, sel1);
-        g3(f3, c, n_sel2),
-        g4(f4, d, sel2);
-    or  g5(f, f1, f2, f3, f4);
-    not g6(n_sel1, sel1);
-    not g7(n_sel2, sel2);
-  end
+  and g1 [31:0] (f1, a, n_sel1),
+      g2 [31:0] (f2, b, sel1);
+      g3 [31:0] (f3, c, n_sel2),
+      g4 [31:0] (f4, d, sel2);
+  or  g5 [31:0] (f, f1, f2, f3, f4);
+  not g6(n_sel1, sel1);
+  not g7(n_sel2, sel2);
 
 endmodule
--- tb_mux.sv
+++ tb_mux.sv
@@ -1,15 +1,15 @@
 `timescale 1ns/1ps
 
 module tb_mux;
-   logic [2:0]count;
-   logic muxOut;
+  logic [1:0] count;
+  logic [31:0] muxOut;
 
-   mux dut(.f(muxOut), .a(count[2]), .b(count[1]), .sel(count[0]));
+  mux dut(.f(muxOut), .a(400), .b(300), .c(200), .d(100), .sel1(count[1]), .sel2(count[0]));
 
-   initial begin
-     $monitor($time,"a = %b | b = %b | sel = %b | muxOut = %b", count[2], count[1], count[0], muxOut);
-     for(count = 0; count != 3'b111; count++) #10;     
-     #10 $stop;
-   end
+  initial begin
+    $monitor($time,"a = %d | b = %d | c = %d | d = %d | sel1 = %b | sel2 = %b | muxOut = %d", 400, 300, 200, 100, count[1], count[0], muxOut);
+    for(count = 0; count != 2'b11; count++) #10;     
+    #10 $stop;
+  end
 
 endmodule: tb_mux
